3524a3861e4b88eb22f79085f8ab161e23bd7d63
Will Haack <wmh1993@gmail.com>
2011-05-11 19:40:08 -0400

    added DumbBot. He just makes random moves

---
+++ DumbBot.java
@@ -0,0 +1,27 @@
+/* the dumb bot just guesses moves */
+public class DumbBot implements TicTacToeBot{
+    int next_move_xcor, next_move_ycor, team;
+    Board gameBoard;
+    
+    /* makes a new ai with access to the gameboard*/
+    public DumbBot(Board newboard, int n){
+        gameBoard = newboard;
+        team = n;
+    }
+    
+    /* continues to guess a move until it gets a valid spot*/
+    public void getNextMove(){
+        next_move_xcor = (int)(Math.random() * 3);
+        next_move_ycor = (int) (Math.random() * 3);
+        while (!gameBoard.canMove(next_move_xcor, next_move_ycor)){
+            next_move_xcor = (int)(Math.random() * 3);
+            next_move_ycor = (int) (Math.random() * 3);
+        }
+    }
+    /* calls getNextMove then makes the move*/
+    public void makeNextMove(){
+        getNextMove();
+        gameBoard.makeMove(next_move_xcor, next_move_ycor, team);
+    }
+    
+}
