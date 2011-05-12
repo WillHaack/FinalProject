09e61db3a2f39892a542dc1d64fabc6fa13892a8
Will Haack <wmh1993@gmail.com>
2011-05-11 20:02:12 -0400

    added clearBoard() method

--- Board.java
+++ Board.java
@@ -83,4 +83,12 @@ public class Board {
     public boolean canMove(int x, int y) {
         return gameboard[x][y] == 0;
     }
+    
+    public void clearBoard(){
+        for (int r = 0; r < 3; r++){
+            for (int c = 0; c < 3; c++){
+                gameboard[r][c] = 0;    
+            }
+        }
+    }
 }
