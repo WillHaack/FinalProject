932fb076f9ec43c0dfc7a0afeb070f890a5196af
Will Haack <wmh1993@gmail.com>
2011-05-12 17:01:24 -0400

    FinalProject now creates a new TicTacToe game, which now creates a new gameboard

--- FinalProject.java
+++ FinalProject.java
@@ -4,9 +4,6 @@ public class FinalProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GameBoard mygame = new GameBoard();
-        DumbBot myBot = new DumbBot(mygame.getBoard(), 2, mygame);
-     for (int i = 0; i < 8; i++)
-         myBot.makeNextMove();
+        TicTacToe newGame = new TicTacToe();
     }
 }
