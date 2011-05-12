01799c1a5764c405d91fbcb41903ecba1dab491f
Graham Carling <Graham@graham-carlings-macbook.local>
2011-05-11 20:11:29 -0400

    Changed a few things in GameBoard, like thickness, added in test X and O pieces

--- GameBoard.java
+++ GameBoard.java
@@ -5,12 +5,20 @@ public class GameBoard extends Frame{
   
   private Line _top, _bottom, _left, _right;
   private Ellipse check;
+  private Xpiece test1;
+  private Opiece test2;
   
   public GameBoard(){
+    test1 = new Xpiece(425,175);
+    test2 = new Opiece(275,25);
     _top = new Line();
     _bottom = new Line();
     _left = new Line();
     _right = new Line();
+    _top.setThickness(2);
+    _bottom.setThickness(2);
+    _left.setThickness(2);
+    _right.setThickness(2);
     _top.setColor(Color.black);
     _bottom.setColor(Color.black);
     _left.setColor(Color.black);
