8dd87790c9286bdf0dd97b005901bf3dcc535a6d
Graham Carling <Graham@graham-carlings-macbook.local>
2011-05-11 20:11:08 -0400

    Created O piece for TicTacToe

---
+++ Opiece.java
@@ -0,0 +1,15 @@
+import wheels.users.*; 
+import java.awt.Color;
+
+public class Opiece{
+  
+  private Ellipse piece;
+  
+  public Opiece(int x, int y){
+    piece = new Ellipse(Color.white);
+    piece.setFrameThickness(2);
+    piece.setFrameColor(Color.black);
+    piece.setSize(130,130);
+    piece.setLocation(x+10,y+10);
+  }
+}
