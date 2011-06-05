a8e5c6c90f038d1c340f2d85a60ccda77f79eff6
yiwaili <yiwai.eric.li@gmail.com>
2011-06-05 08:41:05 -0700

    I rebuilt my old FallDownBoard and FallDown so that it used Java Swing instead of Wheels, since Wheels is retarded and doesn't support keyboard controls.
    This class just extends JFrame, and opens FallDownBoard, which runs the game.

--- FallDown.java
+++ FallDown.java
@@ -1,56 +1,20 @@
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.Color;
-import wheels.users.*;
+import javax.swing.JFrame;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.Timer;
-
-public class FallDown extends Frame{
-  private int score, tick;
-  private boolean isStart;
-  private FallDownBoard board;
-  
-  Timer t = new Timer (100, new ActionListener(){
-    public void actionPerformed(ActionEvent arg0){
-      if (board.getBall().getRow() == 0 || board.getBall().getRow() == board.getBoard().length - 1)
-        t.stop();
-      tick++;
-      score++;
-      if(tick % 10 == 0){
-        board.moveUpBoard();
-        board.moveDown();
-        board.display(210,20,18);
-      }
-      else{
-        board.moveDown();
-        board.display(210,20,18);
-      }
-    }
-  });
+public class FallDown extends JFrame{
   
   public FallDown(){
-    isStart = false;
-    FallBackground fb = new FallBackground();
-    score = 0;
-    tick = 0;
-    board = fb.getBoard();
-    t.start();
+    add(new FallDownBoard(25,15));
+    
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(270, 450);
+    setLocationRelativeTo(null);
+    setTitle("Fall Down");
+    
+    setResizable(false);
+    setVisible(true);
   }
   
   public static void main(String[] args){
-    FallDown app = new FallDown();
-  }
-  public void keyPressed (KeyEvent ke) {
-    switch (ke.getKeyCode()){
-      case KeyEvent.VK_LEFT:
-        board.moveLeft();
-        break;
-      case KeyEvent.VK_RIGHT:
-        board.moveRight();
-        break;
-    }
-    board.display(210,20,18);
+    new FallDown();
   }
 }
